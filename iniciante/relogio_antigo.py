0fa03cb262f70131e2749daed98b159227e25119
Keven Augusto <kevenbezerra@gmail.com>
2021-04-19 18:08:43 -0300

    Altera relogio_antigo.py para as regras da PEP-8

--- iniciante/relogio_antigo.py
+++ iniciante/relogio_antigo.py
@@ -1,8 +1,9 @@
 while True:
     try:
         valores = input().split(' ')
-        print('{:02.0f}:{:02.0f}'
-                                .format(int(valores[0])/30, int(valores[1])/6))
+        hora = int(valores[0])/30
+        minutos = int(valores[1])/6
+        print('{:02.0f}:{:02.0f}'.format(hora, minutos))
 
     except EOFError:
         break
